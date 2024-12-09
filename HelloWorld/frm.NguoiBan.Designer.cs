7e557f3d6ac0a1257222d58440910ed2727442c2
Admin <Admin@THUTHUY>
2024-12-09 21:18:17 +0700

    Add project files.

---
+++ HelloWorld/frm.NguoiBan.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Label label3;
+            label1 = new Label();
+            label2 = new Label();
+            txtMaNB = new TextBox();
+            txtTenNB = new TextBox();
+            label4 = new Label();
+            txtDiaChi = new TextBox();
+            btnThem = new Button();
+            btnCapNhat = new Button();
+            btnXoa = new Button();
+            dataGridView1 = new DataGridView();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(60, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Tên Người Bán";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(340, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Bảng người bán";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Mã Người Bán";
+            // 
+            // txtMaNB
+            // 
+            txtMaNB.Location = new Point(202, 107);
+            txtMaNB.Name = "txtMaNB";
+            txtMaNB.Size = new Size(125, 27);
+            txtMaNB.TabIndex = 0;
+            // 
+            // txtTenNB
+            // 
+            txtTenNB.Location = new Point(202, 166);
+            txtTenNB.Name = "txtTenNB";
+            txtTenNB.Size = new Size(225, 27);
+            txtTenNB.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(61, 222);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Địa Chỉ";
+            // 
+            // txtDiaChi
+            // 
+            txtDiaChi.Location = new Point(202, 226);
+            txtDiaChi.Name = "txtDiaChi";
+            txtDiaChi.Size = new Size(375, 27);
+            txtDiaChi.TabIndex = 2;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(125, 293);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 7;
+            btnThem.Text = "Thêm mới";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnCapNhat
+            // 
+            btnCapNhat.Location = new Point(340, 293);
+            btnCapNhat.Name = "btnCapNhat";
+            btnCapNhat.Size = new Size(94, 29);
+            btnCapNhat.TabIndex = 8;
+            btnCapNhat.Text = "Cập nhật";
+            btnCapNhat.UseVisualStyleBackColor = true;
+            btnCapNhat.Click += btnCapNhat_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(547, 293);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 9;
+            btnXoa.Text = "Xoá";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 360);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 221);
+            dataGridView1.TabIndex = 11;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 593);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnXoa);
+            Controls.Add(btnCapNhat);
+            Controls.Add(btnThem);
+            Controls.Add(txtDiaChi);
+            Controls.Add(label4);
+            Controls.Add(txtTenNB);
+            Controls.Add(label3);
+            Controls.Add(txtMaNB);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Người Bán";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtMaNB;
+        private TextBox txtTenNB;
+        private Label label4;
+        private TextBox txtDiaChi;
+        private Button btnThem;
+        private Button btnCapNhat;
+        private Button btnXoa;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ HelloWorld/frm.NguoiBan.cs
@@ -0,0 +1,354 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        string sCon = "Data Source=THUTHUY\\MSSQLSERVER01;Initial Catalog=NhatNamFood;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Bước 1:
+            SqlConnection con = new SqlConnection(sCon);
+
+            try
+            {
+                con.Open();
+                MessageBox.Show("Kết nối cơ sở dữ liệu thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message, "Thông báo lỗi");
+            }
+
+            //Bước 2: lấy dl về
+            string sQuery = "select * from NguoiBan";
+            SqlDataAdapter adapter = new SqlDataAdapter(sQuery, con);
+
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds, "NguoiBan");
+            dataGridView1.DataSource = ds.Tables["NguoiBan"];
+
+            con.Close();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(sCon);
+
+            try
+            {
+                con.Open();
+                //MessageBox.Show("Kết nối cơ sở dữ liệu thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message, "Thông báo lỗi");
+            }
+            //Chuẩn bị dữ liệu
+            //kiểm tra tính hợp lệ của dl
+
+
+            //gán dl
+            string sMaNB = txtMaNB.Text;
+            string sTenNB = txtTenNB.Text;
+            string sDiaChi = txtDiaChi.Text;
+
+            string sQuery = "insert into NguoiBan values (@MaNB, @TenNB, @DiaChi)";
+            SqlCommand cmd = new SqlCommand(sQuery, con);
+            cmd.Parameters.AddWithValue("@MaNB", sMaNB);
+            cmd.Parameters.AddWithValue("@TenNB", sTenNB);
+            cmd.Parameters.AddWithValue("@DiaChi", sDiaChi);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Thêm mới thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Thêm mới không thành công", "Thông báo");
+            }
+            con.Close();
+        }
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(sCon);
+
+            try
+            {
+                con.Open();
+                //MessageBox.Show("Kết nối cơ sở dữ liệu thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message, "Thông báo lỗi");
+            }
+
+            string sMaNB = txtMaNB.Text;
+            string sTenNB = txtTenNB.Text;
+            string sDiaChi = txtDiaChi.Text;
+
+            string sQuery = "update NguoiBan set TenNB=@TenNB, DiaChi=@DiaChi" + "where MaNB=@MaNB";
+
+            SqlCommand cmd = new SqlCommand(@sQuery, con);
+            cmd.Parameters.AddWithValue("@MaNB", sMaNB);
+            cmd.Parameters.AddWithValue("@TenNB", sTenNB);
+            cmd.Parameters.AddWithValue("@DiaChi", sDiaChi);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Cập nhật người bán thành công", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cập nhật không thành công", "Thông báo");
+            }
+            con.Close();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult ret = MessageBox.Show("Bạn chắc chắn muốn xoá người bán", "Thông báo", MessageBoxButtons.OKCancel);
+            if (ret == DialogResult.OK)
+            {
+
+                SqlConnection con = new SqlConnection(sCon);
+                try
+                {
+                    con.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi kết nối: " + ex.Message, "Thông báo lỗi");
+                }
+                string sMaNB = txtMaNB.Text;
+                string sQuery = "delete NguoiBan where MaNB = @MaNB";
+                SqlCommand cmd = new SqlCommand(@sQuery, con);
+                cmd.Parameters.AddWithValue("@MaNB", sMaNB);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Xoá người bán thành công", "Thông báo");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Xoá người bán không thành công", "Thông báo");
+                }
+                con.Close();
+            }
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaNB.Text = dataGridView1.Rows[e.RowIndex].Cells["MaNB"].Value.ToString();
+            txtTenNB.Text = dataGridView1.Rows[e.RowIndex].Cells["TenNB"].Value.ToString();
+            txtDiaChi.Text = dataGridView1.Rows[e.RowIndex].Cells["DiaChi"].Value.ToString();
+
+            txtMaNB.Enabled = false;
+        }
+    }
+}
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        string sCon = "Data Source=THUTHUY\\MSSQLSERVER01;Initial Catalog=NhatNamFood;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Phương thức kết nối cơ sở dữ liệu
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(sCon);
+        }
+
+        // Phương thức thực thi câu lệnh không trả về dữ liệu
+        private void ExecuteNonQuery(string query, Dictionary<string, object> parameters)
+        {
+            using (var con = GetConnection())
+            {
+                con.Open();
+                using (var cmd = new SqlCommand(query, con))
+                {
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(param.Key, param.Value);
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Phương thức kiểm tra dữ liệu đầu vào
+        private bool ValidateForm()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNB.Text) ||
+                string.IsNullOrWhiteSpace(txtTenNB.Text) ||
+                string.IsNullOrWhiteSpace(txtDiaChi.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo");
+                return false;
+            }
+            return true;
+        }
+
+        // Phương thức xóa dữ liệu trong form
+        private void ClearForm()
+        {
+            txtMaNB.Text = "";
+            txtTenNB.Text = "";
+            txtDiaChi.Text = "";
+            txtMaNB.Enabled = true;
+        }
+
+        // Phương thức tải dữ liệu vào DataGridView
+        private void LoadData()
+        {
+            using (var con = GetConnection())
+            {
+                con.Open();
+                string query = "SELECT * FROM NguoiBan";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "NguoiBan");
+                dataGridView1.DataSource = ds.Tables["NguoiBan"];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadData();
+                MessageBox.Show("Kết nối cơ sở dữ liệu thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: " + ex.Message, "Thông báo lỗi");
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm()) return;
+
+            string query = "INSERT INTO NguoiBan VALUES (@MaNB, @TenNB, @DiaChi)";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@MaNB", txtMaNB.Text },
+                { "@TenNB", txtTenNB.Text },
+                { "@DiaChi", txtDiaChi.Text }
+            };
+
+            try
+            {
+                ExecuteNonQuery(query, parameters);
+                MessageBox.Show("Thêm mới thành công!", "Thông báo");
+                ClearForm();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Thêm mới không thành công: " + ex.Message, "Thông báo lỗi");
+            }
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm()) return;
+
+            string query = "UPDATE NguoiBan SET TenNB=@TenNB, DiaChi=@DiaChi WHERE MaNB=@MaNB";
+            var parameters = new Dictionary<string, object>
+            {
+                { "@MaNB", txtMaNB.Text },
+                { "@TenNB", txtTenNB.Text },
+                { "@DiaChi", txtDiaChi.Text }
+            };
+
+            try
+            {
+                ExecuteNonQuery(query, parameters);
+                MessageBox.Show("Cập nhật thành công!", "Thông báo");
+                ClearForm();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cập nhật không thành công: " + ex.Message, "Thông báo lỗi");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNB.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã người bán để xóa!", "Thông báo");
+                return;
+            }
+
+            DialogResult ret = MessageBox.Show("Bạn chắc chắn muốn xóa người bán này?", "Thông báo", MessageBoxButtons.OKCancel);
+            if (ret == DialogResult.OK)
+            {
+                string query = "DELETE FROM NguoiBan WHERE MaNB = @MaNB";
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@MaNB", txtMaNB.Text }
+                };
+
+                try
+                {
+                    ExecuteNonQuery(query, parameters);
+                    MessageBox.Show("Xóa thành công!", "Thông báo");
+                    ClearForm();
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Xóa không thành công: " + ex.Message, "Thông báo lỗi");
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // Đảm bảo người dùng nhấp vào hàng hợp lệ
+            {
+                txtMaNB.Text = dataGridView1.Rows[e.RowIndex].Cells["MaNB"].Value.ToString();
+                txtTenNB.Text = dataGridView1.Rows[e.RowIndex].Cells["TenNB"].Value.ToString();
+                txtDiaChi.Text = dataGridView1.Rows[e.RowIndex].Cells["DiaChi"].Value.ToString();
+                txtMaNB.Enabled = false;
+            }
+        }
+    }
+}
+
